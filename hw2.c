8dbb22537df11687fa478fea4b03abeff44bb7c6
cherry925 <112562501+cherry925@users.noreply.github.com>
2022-09-15 15:00:04 +0900

    Create hw2.c

---
+++ hw2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	double km;
+	double mile;
+	printf("Please enter kilometers: ");
+	scanf("%lf", &km);
+	mile = km / 1.609;
+	printf("%.1fkm is equal %.1f miles. \n", km, mile);
+	return 0;
+}
